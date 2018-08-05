b114567d07dd57bafb813520b42bad321ef33cec
Sergey Ostapovec <seraphimaion@gmail.com>
2018-08-05 19:47:24 +0300

    upd project

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-
-
-
-})
+  var swipersList = $(".swiper-container");
+  
+  swipersList.each(function (index, item) {
+    var avtoSwiper = new Swiper(item, {
+      speed: 400,
+      spaceBetween: 5,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      }
+    });
+  });
+});
